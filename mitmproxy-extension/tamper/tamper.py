00da4f0cac7c3fc908d5f827aa9e8412fa1505de
Eldad Bercovici <dutzi.b@gmail.com>
2015-08-10 02:00:55 +0300

    Update tamper.py

--- mitmproxy-extension/tamper/tamper.py
+++ mitmproxy-extension/tamper/tamper.py
@@ -28,11 +28,28 @@ try:
     from libmproxy.proxy.server import ProxyServer
     from libmproxy.encoding import decode_gzip
     from libmproxy.proxy.primitives import ProxyServerError
+    import OpenSSL
 except:
     send_message(json.dumps({
         'method': 'proxy-error',
         'errorCode': 101,
         'errorDesc': 'Could not start proxy, error loading libraries'
+    }))
+    sys.exit(1)
+
+version = OpenSSL.__version__.split('.')
+version = {
+    'major': int(version[0]),
+    'minor': int(version[1]),
+    'patch': int(version[2])
+}
+
+if (version['major'] == 0 and version['minor'] < 14):
+    send_message(json.dumps({
+        'method': 'proxy-error',
+        'errorCode': 102,
+        'errorDesc': 'Could not start proxy, pyOpenSSL version is ' + OpenSSL.__version__ +
+            ' (Path: ' + OpenSSL.__file__[0:OpenSSL.__file__.rfind('/')] + ')'
     }))
     sys.exit(1)
 
