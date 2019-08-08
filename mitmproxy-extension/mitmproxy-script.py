cd8096f41b2900ce1d721b2fe0074219beafbd64
Eldad <eldad@spot.im>
2019-08-08 15:39:54 +0300

    add response headers

--- mitmproxy-extension/mitmproxy-script.py
+++ mitmproxy-extension/mitmproxy-script.py
@@ -2,7 +2,7 @@ import json
 import re
 
 
-def get_filename_for_url(fullURL):
+def get_file_object_for_url(fullURL):
 
     with open('/usr/local/bin/tamper_mapping.json') as json_file:
         data = json.load(json_file)
@@ -14,7 +14,7 @@ def get_filename_for_url(fullURL):
         if (re.match(regexURL, fullURL) and url['isEnabled'] == True):
             # send_message(json.dumps(
             #     {'method': 'log', 'message': 'Serving cached file (' + url['cachedFilename'] + ')'}))
-            return url['cachedFilename']
+            return url
 
 
 class ModifyResponse:
@@ -27,16 +27,34 @@ class ModifyResponse:
     def response(self, flow):
         self.num = self.num + 1
         flow.response.headers["Content-Encoding"] = 'identity'
-
-        overridePath = get_filename_for_url(
+        flow.response.status_code = 200
+        flow.response.reason = 'OK'
+        file_object = get_file_object_for_url(
             flow.request.scheme + '://' + flow.request.host + flow.request.path)
 
-        if overridePath:
-            with open('/usr/local/bin/tamper_overrides/' + overridePath) as file:
+        if file_object:
+            with open('/usr/local/bin/tamper_overrides/' + file_object['cachedFilename']) as file:
                 flow.response.content = str(file.read()).encode("utf8")
                 file.close()
 
-            flow.response.headers["via"] = 'tamper'
+            hasViaHeader = False
+            for header in file_object['responseHeaders']:
+                if (header['name'].lower() != 'content-encoding'):
+                    if (header['name'].lower() == 'via'):
+                        hasViaHeader = True
+                        if (header['value'].find('tamper') == -1):
+                            header['value'] += ', tamper'
+
+                    flow.response.headers[str(header['name'])] = str(
+                        header['value'])
+
+            # flow.response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+            # flow.response.headers['Pragma'] = 'no-cache'
+            # flow.response.headers['Expires'] = '0'
+            if (not hasViaHeader):
+                flow.response.headers['via'] = 'tamper'
+
+            # flow.response.headers["via"] = 'tamper'
 
 
 addons = [
